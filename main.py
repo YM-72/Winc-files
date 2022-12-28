0c171fb7dd8de4ed072ce0e640fcb26544a05a8e
YM-72 <84679442+YM-72@users.noreply.github.com>
2022-12-28 12:59:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
 
-import os.path
+import os
+from zipfile import ZipFile
+
+
+base_path = os.getcwd()
+cache_path = os.path.join(base_path, "cache")
+data_path = os.path.join(base_path, "data.zip")
+
  #Part 1
 
-path_cache = os.getcwd()+'\\files'
+path_cache = base_path +'\\files'
 def clean_cache():
     path = path_cache
     if os.path.isdir('files\cache'):
@@ -19,32 +26,27 @@ def clean_cache():
 clean_cache()
 
 #Part 2
-
-from zipfile import ZipFile
-
 def cache_zip(file_name, cache_dir):
-    file_name = 'C:\\Users\\localadmin\\Documents\\Documents\\Learning Code\\Winc\\data.zip'
-    cache_dir = 'C:\\Users\\localadmin\\Documents\\Documents\\Learning Code\\Winc\\cache'
+    file_name = data_path
+    cache_dir = cache_path
     with ZipFile(file_name, 'r') as zip:
         zip.extractall(cache_dir)
 
 #Part 3
-
-
-import os
 def cached_files():
-    dir_path = 'C:\\Users\\localadmin\\Documents\\Documents\\Learning Code\\Winc\\cache'
+    dir_path = cache_path
     list_of_files = []
     for i in os.listdir(dir_path):
         list_of_files.append(os.path.join(dir_path, i))
     return list_of_files
 
 #Part 4
-import os
 def find_password(list_of_files):
     for file_name in list_of_files:
         with open(file_name, 'r') as f:
             contents = f.readlines()
             for line in contents:
                 if 'password' in line:
-                   return line.strip()[10:]
+                     password = line.split(": ")[1][:-1]
+                     print(password)
+                     return password
